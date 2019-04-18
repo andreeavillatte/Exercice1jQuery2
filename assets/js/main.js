25643c6131b4af1704dc6808af49e46af14a684a
core.editor <andreeavillatte@gmail.com>
2019-04-18 14:25:57 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//quand la page est chargée (ready)
+$(document).ready(function () {
+    //si j'appuie sur hide
+    $("#clickMe").click(function () {
+        //je fais disparaitre le paragraphe text
+    alert('Salut! Ça va bien?');
+    });
+});
